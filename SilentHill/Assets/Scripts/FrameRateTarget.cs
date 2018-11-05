e68f4f8c8878ef7f01df20f13ccea3baf581cb3b
Unknown <steviestevens12@gmail.com>
2018-11-04 18:37:46 -0800

    Initialized Unity Project

---
+++ SilentHill/Assets/Scripts/FrameRateTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateTarget : MonoBehaviour {
+
+    [SerializeField]
+    int frameRate = 30;
+
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = frameRate;
+
+    }
+
+    private void Update()
+    {
+        if(Application.targetFrameRate != frameRate)
+        {
+            Application.targetFrameRate = frameRate;
+        }
+    }
+}
---
+++ SilentHill/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame : MonoBehaviour {
+
+
+
+    public void ExitGame()
+    {
+        Debug.Log("fdsafdsa");
+        Application.Quit();
+    }
+}
